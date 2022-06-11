3ab0afd838d0207134179ce491a2b4a13177c1dd
nadimahmed1 <nadimarafat26@gmail.com>
2022-06-11 17:19:07 +0600

    nothing error added

--- js/app.js
+++ js/app.js
@@ -6,6 +6,26 @@ function getInputValue(inputId) {
 }
 // 
 document.getElementById('calculate').addEventListener('click', function () {
+    const income = getInputValue('income-input');
+    const foodInput = getInputValue('food-input');
+    const rentInput = getInputValue('rent-input');
+    const clothInput = getInputValue('cloth-input');
+
+    const expenTotal = document.getElementById('ex-total');
+    expenTotal.innerText = foodInput + rentInput + clothInput;
+
+    const balance = document.getElementById('balance');
+    balance.innerText = income - expenTotal.innerText;
+})
+
+document.getElementById('save-button').addEventListener('click', function () {
+    const saveParsentInput = document.getElementById('save-input');
+    const saveInput = saveParsentInput.value;
+    const savings = parseFloat(saveInput);
+    const income = getInputValue('income-input');
+    const savingAmount = document.getElementById('saving-amount');
+
+    savingAmount.innerText = (income * savings / 100);
 
     const foodInput = getInputValue('food-input');
     const rentInput = getInputValue('rent-input');
@@ -13,5 +33,10 @@ document.getElementById('calculate').addEventListener('click', function () {
 
     const expenTotal = document.getElementById('ex-total');
     expenTotal.innerText = foodInput + rentInput + clothInput;
-})
 
+    const balance = document.getElementById('balance');
+    balance.innerText = income - expenTotal.innerText;
+
+    const remainBalance = document.getElementById('remaining-balance');
+    remainBalance.innerText = balance.innerText - savingAmount.innerText;
+})
